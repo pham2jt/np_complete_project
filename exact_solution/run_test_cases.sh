0324fe9f8fa5ccf7b222e4b40da427e78819b45e
walrus <jtpham00@gmail.com>
2023-04-29 14:54:46 -0400

    bash script added

---
+++ exact_solution/run_test_cases.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+python exact_solution.py < test_cases/input/in1.txt
+python exact_solution.py < test_cases/input/in2.txt
+python exact_solution.py < test_cases/input/in3.txt
+python exact_solution.py < test_cases/input/in4.txt
+python exact_solution.py < test_cases/input/in5.txt
